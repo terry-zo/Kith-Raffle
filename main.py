18dc021c70779ce1a172a8e6a264dc2495451acd
Terry <38963056+zoegodterry@users.noreply.github.com>
2018-05-16 19:18:53 -0400

    Big Update
    -implemented threads

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
 import json
 import sys
+import os
+import platform
 sys.path.append("config")
 sys.path.append("classes")
 from sel_kith import *
 from verifier import *
+from colorama import *
 from Queue import Queue
 from time import time, sleep
 from bs4 import BeautifulSoup as soup
@@ -13,6 +16,17 @@ from threading import Thread, Lock
 
 # Terry Raffle Bot
 # Twitter @zoegodterry
+
+
+def log(phrase, status=None):
+    global GLOBAL_VARIABLES
+    with GLOBAL_VARIABLES["print"]:
+        if status == "Error":
+            print(Fore.RED + phrase)
+        elif status == "Success":
+            print(Fore.GREEN + phrase)
+        else:
+            print(Fore.RESET + phrase)
 
 
 def readconfig(filename):
@@ -69,7 +83,7 @@ def unlock_a(email):
     if email in GLOBAL_VARIABLES["acc_ll"]:
         with GLOBAL_VARIABLES["acc_lock_"]:
             GLOBAL_VARIABLES["acc_ll"].remove(email)
-            print("Unlocked account: " + email)
+            log("Unlocked account: " + email)
 
 
 def grabauthkey(challenge_html):
@@ -83,7 +97,7 @@ def grabauthkey(challenge_html):
 
 
 def get_log_cookie(rand_acc_list, rand_proxy):
-    print("Logging in " + rand_acc_list[0])
+    log("Logging in " + rand_acc_list[0])
     is_verified = verifier(rand_proxy, rand_acc_list[0])
     if is_verified == True:
         with requests.Session() as s:
@@ -120,11 +134,11 @@ def get_log_cookie(rand_acc_list, rand_proxy):
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36"
                 }
                 resp = s.post('https://kith.com/account/login', headers=headers, data=payload, proxies={"https": rand_proxy}, timeout=30)
-                print("Successfully logged in with captcha.")
+                log("Successfully logged in with captcha.", "Success")
                 unlock_p(rand_proxy)
                 return s.cookies
             else:  # no captcha required
-                print("Successfully logged in without captcha.")
+                log("Successfully logged in without captcha.", "Success")
                 unlock_p(rand_proxy)
                 return s.cookies
     else:
@@ -148,14 +162,14 @@ def enter_raffle(accs_tuple, url):
     thread_driver.driver.get(url)
     while GLOBAL_VARIABLES["queue_"].qsize() > 0:
         try:
-            print("Accounts left: " + str(GLOBAL_VARIABLES["queue_"].qsize()))
+            log("Accounts left: " + str(GLOBAL_VARIABLES["queue_"].qsize()))
             with GLOBAL_VARIABLES["q_lock_"]:
                 GLOBAL_VARIABLES["queue_"].get()
             rand_proxy = choice(readproxyfile("config/" + config["proxyfile"]))
             if not rand_proxy in GLOBAL_VARIABLES["p_ll"]:
                 with GLOBAL_VARIABLES["p_lock_"]:
                     GLOBAL_VARIABLES["p_ll"].append(rand_proxy)
-                    print("Using proxy: " + str(rand_proxy))
+                    log("Using proxy: " + str(rand_proxy))
                 if len(accs_tuple) != 0:
                     rand_acc_list = choice(accs_tuple)
                     with GLOBAL_VARIABLES["acc_lock_"]:
@@ -170,7 +184,7 @@ def enter_raffle(accs_tuple, url):
                             rand_sz = GLOBAL_DATA[str(choice(range(2, 20)))]
                             thread_driver.submit_entry(GLOBAL_VARIABLES, rand_sz)
                             if thread_driver.checkentry():
-                                print("Successfully entered raffle.")
+                                log("Successfully entered raffle.", "Success")
                                 thread_driver.refresh()
                                 with GLOBAL_VARIABLES["w_lock"]:
                                     with open("config/Entered.txt", "a+") as etxt:
@@ -178,32 +192,33 @@ def enter_raffle(accs_tuple, url):
                                     with open("config/Entered_Detailed.txt", "a+") as etxt:
                                         etxt.write("{}:{}:{}:{}\n".format(rand_acc_list[0], rand_acc_list[1], rand_sz, GLOBAL_VARIABLES["actual_loc"]))
                             else:
-                                print("Failed to enter raffle.")
+                                log("Failed to enter raffle.", "Error")
                                 error_restart(thread_driver, rand_acc_list)
                         else:
-                            print("Account already entered into raffle.")
+                            log("Account already entered into raffle.", "Error")
                     elif log_cookies_ == "banned":
                         with GLOBAL_VARIABLES["acc_lock_"]:
                             accs_tuple = accs_tuple + tuple(rand_acc_list)
                 else:
-                    print("Selected proxy is in use.")
-                    with GLOBAL_VARIABLES["q_lock_"]:
-                        GLOBAL_VARIABLES["queue_"].put(1)
+                    log("Finished all accounts.", "Success")
+                    sys.exit()
             else:
-                print("Finished all accounts.")
-                sys.exit()
+                log("Selected proxy is in use.", "Error")
+                with GLOBAL_VARIABLES["q_lock_"]:
+                    GLOBAL_VARIABLES["queue_"].put(1)
         except Exception:
-            print("Caught an error.")
+            log("Caught an error.", "Error")
             with GLOBAL_VARIABLES["q_lock_"]:
                 GLOBAL_VARIABLES["queue_"].put(1)
             accs_tuple = accs_tuple + tuple(rand_acc_list)
+    thread_driver.end_session()
 
 
 def captcha_harvester():
     with requests.session() as c_s:
         captcha_id = request_recaptcha(c_s)
         token = receive_token(captcha_id, c_s)
-        print("Token received.")
+        log("Token received.")
         return token
 
 
@@ -212,24 +227,24 @@ def request_recaptcha(session):
     url = "http://2captcha.com/in.php?key=" + GLOBAL_VARIABLES["service_key"] + "&method=userrecaptcha&googlekey=" + GLOBAL_VARIABLES["google_site_key"] + "&pageurl=" + GLOBAL_VARIABLES["captcha_url"]
     resp = session.get(url)
     if resp.text[0:2] != 'OK':
-        print("Error: {} Exiting...".format(resp.text))
+        log("Error: {} Exiting...".format(resp.text), "Error")
     captcha_id = resp.text[3:]
-    print("Successfully requested for captcha.")
+    log("Successfully requested for captcha.", "Success")
     return captcha_id
 
 
 def receive_token(captcha_id, session):
     global GLOBAL_VARIABLES
     fetch_url = "http://2captcha.com/res.php?key=" + GLOBAL_VARIABLES["service_key"] + "&action=get&id=" + captcha_id
+    log("Waiting for captcha.")
     for count in range(1, 26):
-        print("Attempting to fetch token. {}/25".format(count))
         resp = session.get(fetch_url)
         if resp.text[0:2] == 'OK':
             grt = resp.text.split('|')[1]  # g-recaptcha-token
-            print("Captcha token received.")
+            log("Captcha token received.", "Success")
             return grt
         sleep(5)
-    print("No tokens received. Restarting...")
+    log("No tokens received. Restarting...", "Error")
     receive_token(captcha_id, session)
 
 
@@ -240,8 +255,17 @@ def wrapper_(accs_tuple, url):
         with GLOBAL_VARIABLES["q_lock_"]:
             GLOBAL_VARIABLES["queue_"].put(index)
     st = time()
-    enter_raffle(accs_tuple, url)
-    print("Finished in " + str(time() - st))
+    threads = []
+    for _ in range(10):
+        t = Thread(target=enter_raffle, args=(accs_tuple, url))
+        threads.append(t)
+    [t.start() for t in threads]
+    [t.join() for t in threads]
+    if str(platform.system()) == "Windows":
+        log("Cleaning up any left over drivers...")
+        os.chdir("classes")
+        os.system("killcdriver.bat")
+    log("Finished in " + str(time() - st), "Success")
 
 
 if __name__ == "__main__":
@@ -252,6 +276,7 @@ if __name__ == "__main__":
         "q_lock_": Lock(),
         "acc_lock_": Lock(),
         "p_lock_": Lock(),
+        "print": Lock(),
         "acc_ll": [],
         "p_ll": [],
         "queue_": Queue(),
